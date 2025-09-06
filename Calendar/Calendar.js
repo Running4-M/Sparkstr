5c57901b2b4d82ec35c7c18ee5b5ece51f99c192
Running4-M <mchlrotaru@gmail.com>
2025-09-06 21:42:48 +0100

    Update Calendar.js

--- Calendar/Calendar.js
+++ Calendar/Calendar.js
@@ -111,7 +111,7 @@ window.__tutorialEventSaved__ = function() {
 
 firebaseInitPromise.then(() => {
   if (!getCurrentUserId()) {
-    window.location.href = "../Login/signup.html";
+    window.location.href = "../Login/signup";
   }
 });
 
@@ -123,7 +123,7 @@ async function loadTemplates() {
 
   try {
     console.log("📥 Fetching templates...");
-    const response = await fetch('./templates.html');
+    const response = await fetch('./templates');
     if (!response.ok) throw new Error('Failed to load templates');
 
     const templatesHTML = await response.text();
@@ -4943,6 +4943,7 @@ document.getElementById('mobileSidebarHamburger').addEventListener('click', func
   }
 });
     
+
 
 
 
