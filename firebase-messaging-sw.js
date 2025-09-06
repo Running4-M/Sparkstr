b7d7d31008ae9948f17ce7e26c5e84f61187db84
Running4-M <mchlrotaru@gmail.com>
2025-09-06 19:58:49 +0100

    Add files via upload

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,28 @@
+importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js");
+
+// Initialize Firebase in the service worker (REQUIRED for FCM to work here)
+firebase.initializeApp({
+  apiKey: "AIzaSyC9MoRFgajbAt58_s2zuW6vW6QKzpzUIbc",
+  authDomain: "sparkstr.com",
+  projectId: "ai-calendar-5753a",
+  storageBucket: "ai-calendar-5753a.appspot.com",
+  messagingSenderId: "610949624500",
+  appId: "1:610949624500:web:b63a91859c298bb0e7dde1",
+  measurementId: "G-8JTTER2Z6T"
+});
+
+
+// Get messaging instance
+const messaging = firebase.messaging();
+
+// Handle background messages
+messaging.onBackgroundMessage(function(payload) {
+  const notificationTitle = payload.notification?.title || 'Background Message Title';
+  const notificationOptions = {
+    body: payload.notification?.body || 'Background Message body.',
+    icon: '/icon-192.png'
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
